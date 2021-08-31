3654e64e5ee5a851f6e653d80b0c0207a9cb1d50
5unagawa <s2885524@griffithuni.edu.au>
2021-09-01 08:20:55 +0900

    Update hangman.js

--- hangman.js
+++ hangman.js
@@ -1,35 +1,39 @@
-var targetWord; //word the player will guess
-var blankWord;
-var chances;
-var letters = /^[A-Za-z]+$/;
+var targetWord; //word that the player will guess
+var blankWord;  //array to hold blank spaces
+var chances;    //counter for number of chances left
+var letters = /^[A-Za-z]+$/; //input verification
 
-//disable input buttons, re-enable start button
+//disable buttons on page load
 document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = true;});
 
+//button click event
 const keys = document.querySelector('.btn-group');
 keys.addEventListener('click', (event) => {
-	//access clicked element
+  //target clicked button
 	const {target} = event;
 	
-	//check clicked element is button or 
+	//check that clicked element is a button. 
 	//if not, exit function
 	if (!target.matches('button')) {
 		return;
 	}
 	
-	//else
+	//run checkLetter fucntion against key pressed
+  //hide the button and update output
 	checkLetter(target.value);
 	target.style.visibility = "hidden";
 	document.getElementById("word").innerHTML = blankWord.join(' ');
 	console.log(blankWord);
-	
 });
 
-function startGame(){ //get user input and prepare game board
+//initializes the game
+function startGame(){
+  //reset the word to be guessed and disabled buttons
 	document.getElementById("word").innerHTML = "";
 	document.querySelectorAll('.btn-group button').forEach(elem=> {elem.style.visibility = "visible";});
-	let input = prompt("Please enter your word:", "...");
-	
+  
+	//ask user for input and prepare a blank version of the word
+  let input = prompt("Please enter your word:", "...");
 	if (input.match(letters)){ //add check for symbols and numbers
 		targetWord = input;
 		blankWord = new Array(targetWord.length);
@@ -39,16 +43,20 @@ function startGame(){ //get user input and prepare game board
 		console.log("Ready to play");
 		document.getElementById("word").innerHTML = blankWord.join(' ');
 		console.log(blankWord);
-		chances = 10;
+		
+    //reset chance counter and images
+    chances = 10;
 		document.getElementById("chance").innerHTML = "Chances: " + chances;
-document.getElementById("chanceImg").src=("images/number_" + chances + ".png");
+    document.getElementById("chanceImg").src=("image/" + chances + ".png");
 		document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = false;});
+    //hide start button
 		document.getElementById("start-btn").style.visibility="hidden";
+    
 	} else {
 		window.alert("Please enter your word again.");
 	}
 }
-
+//compare clicked button value against letters in word
 function checkLetter(input){
 	console.log(input);
 	let foundFlag = 0;
@@ -56,29 +64,31 @@ function checkLetter(input){
 		if (input == targetWord[i].toUpperCase()) {
 			blankWord[i] = input;
 			foundFlag = 1
-			
 		}
 	}
 	
+  //if no match found
+  //reduce counter and change image
 	if (foundFlag == 0) {
 		--chances;
-		document.getElementById("chanceImg").src=("images/number_" + chances + ".png");
+		document.getElementById("chanceImg").src=("image/" + chances + ".png");
 		document.getElementById("chance").innerHTML = "Chances: " + chances;
-
-		if (chances == 0) {
-			window.alert("Out of chances. Game Over. The word was: " + targetWord);
-			document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = true;});
 	
-			document.getElementById("start-btn").style.visibility="visible";
-			
+  //if player runs out of guesses
+  //alert answer, disable buttons and re-enable start button
+  if (chances == 0) {
+	  window.alert("Out of chances. Game Over. The word was: " + targetWord);
+		document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = true;});
+		document.getElementById("start-btn").style.visibility="visible";		
 		}
 	}
 	
+  //check that all letters have been found
+  //alert player, disable buttons and re-enable start button
 	if (blankWord.includes("_") == false) {
 		document.getElementById("word").innerHTML = blankWord.join(' ');
 		window.alert("Congratulations! You win!");
 		document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = true;});
-
 		document.getElementById("start-btn").style.visibility="visible";
 	}
 }

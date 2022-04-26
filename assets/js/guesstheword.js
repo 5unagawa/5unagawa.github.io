7706edbf7b5ed394fa4401efde820facfd2e3608
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-04-26 10:02:54 +0900

    Update guesstheword.js

--- assets/js/guesstheword.js
+++ assets/js/guesstheword.js
@@ -1,6 +1,8 @@
 var targetWord; //word that the player will guess
+var blankWord;  //array to hold blank spaces
 var chances;    //counter for number of chances left
 var prevNum;    //holds value of previous question
+var letters = /^[A-Za-z]+$/; //input verification
 
 var fruitArray = ["APPLE", "LEMON", "MANGO", "MELON", "PEACH"];
 let guessArray = [];
@@ -10,21 +12,20 @@ document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = t
 //button click event
 const keys = document.querySelector('.btn-group');
 keys.addEventListener('click', (event) => {
-	console.log("button clicked");
   //target clicked button
-   const {target} = event;
+	const {target} = event;
+  
    switch(target.matches('button')){
   	case target.id == 'enter-btn' :
-    	    break;
+    	break;
    	case target.id == 'back-btn' :
-    	    break;
-        default:
-    	    console.log("fire");
-            if(guessArray.length < 5){
-      	        guessArray.push(target.value);
-      	        document.getElementById('word_'+chances).innerHTML = guessArray.join(' ');
-      	        console.log(guessArray);
-            }
+    	break;
+    default:
+    	if(guessArray.length < 5){
+      	guessArray.push(target.value)
+      	document.getElementById('word_'+chances).innerHTML = guessArray.join(' ');
+      	console.log(guessArray);
+  		}
   }
 });
 
@@ -51,11 +52,15 @@ function startGame(){
   document.querySelectorAll('.btn-group button').forEach(elem=> {elem.style.visibility = "visible";});
 
   chooseImage();
+	
+  blankWord = new Array(targetWord.length);
+  for (let i=0; i < targetWord.length; i++) {
+      blankWord[i] = "_";
+  }
   console.log("Ready to play");
-//  document.getElementById('word_'+chances).innerHTML = blankWord.join(' ');
-  		
-  //reset chance counter and images
-  document.getElementById("chance").innerHTML = "Chances: " + chances;
+  document.getElementById('word_'+chances).innerHTML = blankWord.join(' ');
+  console.log(blankWord);
+
   document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = false;});
 }
 
@@ -68,60 +73,73 @@ function checkGuess(g){
     }
 
 		else {
- let guessResults = new Array(5);
-    let myGuess = g;
-    for (let i = 0; i < 5; i++){
-        //if direct match
-        console.log("comparing: " + myGuess[i] + " and " + targetWord[i]);
-      	if (myGuess[i] == targetWord[i]){
-        		console.log("direct match found");
-            guessResults[i] = 1; // 1 denotes direct match
-         }
-    	    else for (let j = 0; j < 5; j++){
-           console.log("comparing: " + myGuess[i] + " and " + targetWord[j]);
-                //if indirect match
-                if (myGuess[i] == targetWord[j]){
-		    if (guessResults[i] != 1 || guessResults != 2){
-          		guessResults[i] = 2; //2 denotes indirect match
-              console.log("indirect match");
-			break;
-		    }
-		    else
-        		console.log("indirect match already found");
-		        break;
-        	}
-		else
-    		console.log("no match found");
-		    guessResults[i] = 0;
-            }
-    	}    
-    
-    console.log(guessResults);
-		//update colours
-		styleOutput(guessArray, guessResults);
-    }
+			let guessResults = new Array(5);
+			let myGuess = g;
+			for (let i = 0; i < 5; i++){
+				//if direct match
+				console.log("comparing: " + myGuess[i] + " and " + targetWord[i])
+				if (myGuess[i] == targetWord[i]){
+					console.log("direct match found");
+					guessResults[i] = 1; // 1 denotes direct match
+				}
+				else for (let j = 0; j < 5; j++){
+					console.log("comparing: " + myGuess[i] + " and " + targetWord[j]);
+					//if indirect match
+					if (myGuess[i] == targetWord[j]){
+						if (guessResults[i] != 1 || guessResults != 2){
+							guessResults[i] = 2; //2 denotes indirect match
+							console.log("indirect match");
+							break;
+						}
+						else
+							console.log("indirect match already found");
+							break;
+					}
+					else
+					console.log("no match found");
+					guessResults[i] = 0;
+				}	
+			}    
+			console.log(guessResults);
+			//update colours
+			styleOutput(guessArray, guessResults);
+			
+			
+			let result = guessResults.every(function (e){
+				return e == 1;
+			});
+			if (result == true){
+				console.log("got to here");
+				alert("well done");
+				//disable buttons
+				document.querySelectorAll('.btn-group button').forEach(elem=> {elem.disabled = true;});
+				resetBoard();
+			}
+		}
+	
     guessArray = [];
     --chances;
-
+	
     if (chances == 0){
-	alert("game over");
-	resetBoard();
+		alert("game over");
+		resetBoard();
     }
 }
 
 function backspace(){
-    guessArray.pop();
+    guessArray.pop()
     document.getElementById('word_'+chances).innerHTML = guessArray.join(' ');
 }
 
 //Loops through the array and creates a text node and span for each letter that is used to apply styling. Appends the element to the end of the array which has been cleared.
 function styleOutput(gA, gR){
 	var res = gR;
+	var playerGuess = gA;
 	var myWord = document.getElementById('word_' + chances);
   myWord.innerHTML = "";
   for (let i = 0; i < 5; i++){
   	var elem = document.createElement("span"),
-        text = document.createTextNode(gA[i] + " ");
+        text = document.createTextNode(playerGuess[i] + " ");
     elem.appendChild(text);
     if (res[i] == 1){
         console.log("changing color g");
@@ -133,14 +151,31 @@ function styleOutput(gA, gR){
     }
   myWord.appendChild(elem);
   }
+  restyleBoard(playerGuess, res)
+}
+
+function restyleBoard(gA, gR){
+	for (let i = 0; i < 5; i++){
+		var guessedLetterKey = document.getElementById(gA[i]);
+		if (gR[i] == 0){
+			guessedLetterKey.style.backgroundColor = "red";
+		}
+		if (gR[i] == 1){
+			guessedLetterKey.style.backgroundColor = "green";
+		}
+		if (gR[i] == 2){
+			guessedLetterKey.style.backgroundColor = "yellow";
+		}			
+	}
 }
 
 function resetBoard(){
 	document.getElementById("enter-btn").setAttribute( "onClick", "startGame()");
   	document.getElementById("enter-btn").innerHTML = "NEW GAME";
   	document.getElementById("enter-btn").id = "start-btn";
+	document.querySelectorAll('.btn-group button').forEach(elem=> {elem.style.backgroundColor = "#248232";});
 
 	for (let i = 6; i > 0; i--){
-		document.getElementById("word_" + i).innerHTML = " ";
+		document.getElementById("word_" + i).innerHTML = "_ _ _ _ _";
 	}
 }

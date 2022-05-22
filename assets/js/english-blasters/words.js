1d6923a310e249851ce0213b82ca24c649e3e8af
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-05-22 13:21:16 +0900

    Create words.js

---
+++ assets/js/english-blasters/words.js
@@ -0,0 +1,27 @@
+var dictionary = [ {jp:"黒", en:"black"}, {jp:"青", en:"blue"},
+	{jp:"茶色", en:"brown"},
+	{jp:"緑", en:"green"},
+	{jp:"灰色", en:"gray"},
+	{jp:"オレンジ", en:"orange"},
+	{jp:"ピンク", en:"pink"},
+	{jp:"紫", en:"purple"},
+	{jp:"赤", en:"red"},
+	{jp:"白", en:"white"},
+	{jp:"黄色", en:"yellow"}
+];
+
+function createWordList(wordCount)
+  let wordList = [wordCount];
+  for (let i = 0; i < wordCount; i++){
+    wordList[i] = {xPos: 0, yPos: 0, answer: chooseWords(dictionary)}; 
+  }
+return wordList;
+
+//Randomly selects a word object from wordArray
+function chooseWords(wordArray){
+  let randomNum = Math.floor(Math.random() * wordArray.length);
+  let randomWordObj = wordArray[randomNum];
+  return randomWordObj;
+}
+
+export { createWordList, chooseWords };

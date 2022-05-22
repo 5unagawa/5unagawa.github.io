c863212dd47d037b794c98ad76364dc80499ec46
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-05-22 12:07:11 +0900

    Create tank.js

---
+++ assets/js/english-blasters/tank.js
@@ -0,0 +1,66 @@
+//tank.js
+
+function createTank(myCanvas){
+  let canvas = myCanvas;
+  let tankObj = {width: 30, height: 10, position: (myCan.width - tankObj.width)/2 } 
+  return tankObj;
+}
+
+function drawTank(tankObj, myCanvas){
+  let canvas = myCanvas;
+  let ctx = canvas.getContext('2d');
+  let tank = tankObj;
+  
+  //draw body of tank
+  ctx.beginPath();
+  ctx.rect(tank.position, canvas.height - tank.height, tank.width, tank.height);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+  
+  //draw turret
+  ctx.beginPath();
+  ctx.rect(tank.position + 10, canvas.height - tank.height - 10, 10, 10);
+  ctx.fillStyle = "red";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function moveTank(myCanvas, tankObj, left, right){
+  let canvas = myCanvas;
+  let tank = tankObj;
+  let newPos = tank.position;	
+  if (left == true){
+    if (tank.position  <= 0) {
+        newPos = 0;
+    }
+    else newPos -= 5;
+  }
+	
+  if (right == true){
+    if (tank.position + tank.wodth >= canvas.width) {
+        newPos = canvas.width - tank.width;
+    }
+    else  newPos += 5;
+  }
+  return newPos;
+}
+
+function createBullet(myCanvas, tankObj){
+	let canvas = myCanvas;
+  let tank = tankObj;
+  let bulletObj = {spawn: canvas.height - (tank.height + 15), xPos: tank.position, yPos: canvas.height - (tank.height + 15), active: false};
+	return bulletObj;
+}
+
+function drawBullet(myCanvas, bulletObj){
+  let canvas = myCanvas;
+  let ctx = canvas.getContext('2d');
+  let bullet = bulletObj;
+  
+  ctx.beginPath();
+  ctx.arc(bullet.xPos, bullet.yPos, 5, 0, Math.PI*2);
+  ctx.fillStyle = "white";
+  ctx.fill();
+  ctx.closePath();
+}

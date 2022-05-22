c100f8ab7181d0067907c1193254b8ff6d88544b
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-05-22 15:36:11 +0900

    Update words.js

--- assets/js/english-blasters/words.js
+++ assets/js/english-blasters/words.js
@@ -13,7 +13,7 @@ var dictionary = [ {jp:"黒", en:"black"}, {jp:"青", en:"blue"},
 function createWordList(wordCount){
   let wordList = [];
   for (let i = 0; i < wordCount; i++){
-    wordList[i] = {xPos: 0, yPos: 0, answer: chooseWords(dictionary)}; 
+    wordList[i] = {xPos: 0, yPos: 0, answer: chooseWords(dictionary), status: 1}; 
   }
   return wordList;
 }

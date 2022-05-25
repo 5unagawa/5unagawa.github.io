b81e87169831f62b1fcbf198f87adddcdae27af3
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-05-25 15:17:52 +0900

    Update words.js

--- assets/js/english-blasters/words.js
+++ assets/js/english-blasters/words.js
@@ -10,17 +10,26 @@ var dictionary = [ {jp:"黒", en:"black"}, {jp:"青", en:"blue"},
 	{jp:"黄色", en:"yellow"}
 ];
 
+//set up the word list
+function initDictionary(){
+    let wordList = dictionary;
+    shuffle(wordList);
+    return wordList; 	
+}
+
 //Shuffles the dictionary using the Fisher-Yates / Durstenfeld Shuffle.
-function shuffle(array){
-  for (let i = array.length - 1; i > 0; i--){
+//This is usedhelp ensure that getNewWords does not select the same word more than once each round.
+function shuffle(dict){
+  for (let i = dict.length - 1; i > 0; i--){
     let j = Math.floor(Math.random() * (i + 1));
-    let temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
+    let temp = dict[i];
+    dict[i] = dict[j];
+    dict[j] = temp;
   }
 }
 //Uses the first 3 words in the shuffled dictionary array to create a new list
-function createWordList(shuffledDict){
+//While the same sets of 3 words can potentially appear, the same word will not appear more than once each round.
+function getNewWords(shuffledDict){
   let wordList = [];
   for (let i = 0; i < wordCount; i++){
     wordList[i] = {xPos: 0, yPos: 0, answer: shuffledDict[i], status: 1};
@@ -34,10 +43,10 @@ function createWordList(shuffledDict){
   return wordList;
 }
 
-function chooseWords(wordArray){
-  let randomNum = Math.floor(Math.random() * wordArray.length);
-  let randomWordObj = wordArray[randomNum];
+function chooseTargetWord(wordList){
+  let randomNum = Math.floor(Math.random() * wordList.length);
+  let randomWordObj = wordList[randomNum];
   return randomWordObj;
 }
 
-export { createWordList, chooseWords, shuffle };
+export { initDictionary, shuffle, getNewWords, chooseTargetWord };

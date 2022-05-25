f5315a5f39e5da016fe0e1b3eae8d4f1941dcb25
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-05-25 15:06:54 +0900

    Update words.js

--- assets/js/english-blasters/words.js
+++ assets/js/english-blasters/words.js
@@ -10,19 +10,34 @@ var dictionary = [ {jp:"黒", en:"black"}, {jp:"青", en:"blue"},
 	{jp:"黄色", en:"yellow"}
 ];
 
-function createWordList(wordCount){
+//Shuffles the dictionary using the Fisher-Yates / Durstenfeld Shuffle.
+function shuffle(array){
+  for (let i = array.length - 1; i > 0; i--){
+    let j = Math.floor(Math.random() * (i + 1));
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+}
+//Uses the first 3 words in the shuffled dictionary array to create a new list
+function createWordList(shuffledDict){
   let wordList = [];
   for (let i = 0; i < wordCount; i++){
-    wordList[i] = {xPos: 0, yPos: 0, answer: chooseWords(dictionary), status: 1}; 
+    wordList[i] = {xPos: 0, yPos: 0, answer: shuffledDict[i], status: 1};
+    
+    //the word at index[0] is moved to the back of the array
+    let temp = shuffledDict[i];
+    shuffledDict.shift();
+    shuffledDict.push(temp);
   }
+  //move the 3 chosen words to the back of the array
   return wordList;
 }
 
-//Randomly selects a word object from wordArray
 function chooseWords(wordArray){
   let randomNum = Math.floor(Math.random() * wordArray.length);
   let randomWordObj = wordArray[randomNum];
   return randomWordObj;
 }
 
-export { createWordList, chooseWords };
+export { createWordList, chooseWords, shuffle };

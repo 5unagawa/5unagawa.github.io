064820c268f00f72598280f9adbcb57c2c71b513
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-06-14 09:46:50 +0900

    Create UFODictionaries.js

---
+++ modules/UFODictionaries.js
@@ -0,0 +1,6 @@
+var thirdGradeDictionary = [
+    "affect", "asleep", "bell", "body", "brain", "bright", "cross", "concentrate", "daytime", "energy",
+    "habit", "held", "improve", "lack", "lose", "lost", "minute", "nap", "result", "screen",
+    "sign", "sir", "tonight", "walker" 
+]
+export {thirdGradeDictionary};

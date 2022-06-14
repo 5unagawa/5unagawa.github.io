6c14b3d96f3b7899072295947bfd6a35011210d3
Daniel Sandstrom <7506834+5unagawa@users.noreply.github.com>
2022-06-14 10:07:05 +0900

    Update UFODictionaries.js

--- assets/modules/UFODictionaries.js
+++ assets/modules/UFODictionaries.js
@@ -2,7 +2,7 @@ var thirdGradeDictionary = [
     "affect", "asleep", "bell", "body", "brain", "bright", "cross", "concentrate", "daytime", "energy",
     "habit", "held", "improve", "lack", "lose", "lost", "minute", "nap", "result", "screen",
     "sign", "sir", "tonight", "walker" 
-]
+];
 
 //set up the word list
 function initDictionary(){
